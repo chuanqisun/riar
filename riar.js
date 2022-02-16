dfb9860a32785009c949403b86bbc06de6ce4b96
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2022-02-15 19:24:04 -0800

    allow save

--- riar.js
+++ riar.js
@@ -15,8 +15,48 @@ document.getElementById("edit").onclick = async () => {
   const parser = new DOMParser();
   const dom = parser.parseFromString(contentStr, "text/html");
   document.getElementById("editable").innerHTML =
-    dom.getElementById("editable");
-  return;
+    dom.getElementById("editable").innerHTML;
+};
+
+document.getElementById("save").onclick = async () => {
+  const contentObj = await (
+    await fetch(
+      `https://api.github.com/repos/${localStorage.getItem(
+        "ghPath"
+      )}/contents/home.html?${Date.now()}`,
+      {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: `token ${localStorage.getItem("ghToken")}`,
+      }
+    )
+  ).json();
+
+  const sha = atob(contentObj.sha);
+
+  const contentStr = atob(contentObj.content);
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(contentStr, "text/html");
+  dom.getElementById("editable").innerHTML =
+    document.getElementById("editable").innerHTML;
+  const newContent = btoa(dom.documentElement.outerHTML);
+
+  await fetch(
+    `https://api.github.com/repos/${localStorage.getItem(
+      "ghPath"
+    )}/contents/home.html`,
+    {
+      method: "PUT",
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: `token ${localStorage.getItem("ghToken")}`,
+      },
+      body: JSON.stringify({
+        message: "bot generated",
+        content: newContent,
+        sha,
+      }),
+    }
+  );
 };
 
 window.addEventListener("keydown", (e) => {
@@ -28,22 +68,5 @@ window.addEventListener("keydown", (e) => {
       localStorage.setItem("ghPath", path);
       localStorage.setItem("ghToken", token);
     }
-  }
-});
-
-window.addEventListener("keydown", (e) => {
-  if (e.ctrlKey && e.code === "KeyS") {
-    e.preventDefault();
-    // fetch(window.location.href)
-    //   .then((result) => result.text())
-    //   .then((html) => {
-    //     const parser = new DOMParser();
-    //     const dom = parser.parseFromString(html, "text/html");
-    //     dom.body.innerHTML = document.getElementById("editable").innerHTML;
-    //     return dom.documentElement.innerHTML;
-    //   })
-    //   .then((html) => {
-    //     console.log(html);
-    //   });
   }
 });
